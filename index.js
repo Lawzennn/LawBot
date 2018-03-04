d874e94209ce784784e0256d7e3fb46f2c70968d
Lawzennn <37016897+Lawzennn@users.noreply.github.com>
2018-03-04 11:40:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
+const bot = new Discord.Client({disableEveryone: true});
 
-var bot = new Discord.Client();
 var prefix = ("/");
 
-  bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: '/help ByLawzenn ©', type: 0} });
-    console.log("Je suis connecté !");
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+
+  bot.user.setActivity("(BETA)", {type: "WATCHING"});
+
+  //bot.user.setGame("on SourceCade!");
 });
-
-
-bot.login(process.env.BOTDISCORD_TOKEN);
-
-bot.on('message', message => {
-    if (message.content === '/couleur'){
-        message.reply("Voici les couleurs :@Red @Cyant @Blue @Violet @Pink @Yellow @Orange @Grey @Black @White");
-        console.log('ping pong')
-};
-
-
-
-  if (message.content === prefix + "help"){
-      var help_embed = new Discord.RichEmbed()
-          .setColor('#ff8888')
-          .addField("Commande de Respect des règles - ", "  -/commande : Affiche les commandes du bot !\n-/Ban @pseudo SOON *\n-/tempban @pseudo SOON * \n-/kick @pseudo SOON* \n-/mute @pseudo SOON * \n-/tempmute @pseudo SOON *")
-          .addField("Interaction - ", "  -/ping le bot te repond pong !\n-/salut le bot te repond ça va ?")
-          .addField("Information - ", "  -/discord\n-/support\n-/version\n-/language")
-          .addField("Couleurs - ", "  -Les couleurs disponible pour vous etre attribuer en faisant /couleur (couleur)\n@Red @Cyant @Blue @Violet @Pink @Yellow @Orange @Grey @Black @White")
-          .setFooter("Bot Programmé par Lawzenn")
-      message.channel.sendEmbed(help_embed);
-      //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
-      console.log("Commande Help demandé !");
-    }
-
-  if (message.content === prefix + "ban"){
-      var help_embed = new Discord.RichEmbed()
-          .setColor('#F30000')
-          .addField("Bannisement", " :x:-/ban arrive bientot !")
-          .setFooter("Bot Programmé par Lawzenn")
-      message.channel.sendEmbed(help_embed);
-      //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
-      console.log("Commande Ban demandé !");
-    }
-
-  if (message.content === prefix + "kick"){
-      var help_embed = new Discord.RichEmbed()
-          .setColor('#F30000')
-          .addField("Exclusion", " :x:-/kick arrive bientot !")
-          .setFooter("Bot Programmé par Lawzenn")
-        message.channel.sendEmbed(help_embed);
-        //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
-        console.log("Commande Kick demandé !");
-      }
-
-  if (message.content === prefix + "tempban"){
-      var help_embed = new Discord.RichEmbed()
-          .setColor('#F30000')
-          .addField("Temps-Bannissement", " :x:-/tempban arrive bientot !")
-          .setFooter("Bot Programmé par Lawzenn")
-        message.channel.sendEmbed(help_embed);
-        //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
-          console.log("Commande Tempban demandé !");
-        }
-
-        if (message.content === prefix + "discord"){
-            var help_embed = new Discord.RichEmbed()
-                .setColor('#F30000')
-                .addField("Discord", "  -Voici le discord https://discord.gg/AjXnwCX ")
-                .setFooter("Bot Programmé par Lawzenn")
-              message.channel.sendEmbed(help_embed);
-              //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
-                console.log("Commande Discord demandé !");
-              }
-
-              if (message.content === prefix + "salut"){
-                var help_embed = new Discord.RichEmbed()
-                    .setColor('#F30000')
-                    .addField("Salutation", "  -Tout va bien pour vous ?")
-                    .setFooter("Bot Programmé par Lawzenn")
-                  message.channel.sendEmbed(help_embed);
-                  //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
-                    console.log("Commande Salut demandé !");
-                  }
-            
-    });
-
 
 bot.on("message", async message => {
   if(message.author.bot) return;
@@ -212,3 +138,5 @@ bot.on("message", async message => {
   }
 
 });
+
+bot.login(process.env.BOTDISCORD_TOKEN);
